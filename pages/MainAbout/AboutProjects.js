5354a33abb57e2b4ede6259db1dd860e36d61bb7
bytes <91709492+dev-divi@users.noreply.github.com>
2022-02-06 17:58:23 -0500

    Changed About

--- pages/MainAbout/AboutProjects.js
+++ pages/MainAbout/AboutProjects.js
@@ -1,14 +1,16 @@
 const AboutProjects = () => {
     return ( 
       <p>
-        {/* 🧊 Mystery Cube Club */} 
+        <a href="https://0xdivi.art/">💡0xDivi.art</a>  
+
+        {/* 🧊 Mystery Cube Club 
         <a href="https://lightdark.art/">💡lightdark</a>  
         <br /> 
         <a href="https://heroorvillain.io/">😇hero or villain😈 [unreleased]</a>  
         <br /> 
         💴<a href="https://sorcerawr.com/theupdater2">updater</a>  
         <br /> 
-        🔮<a href="https://divinescripture.art/">scripture entropy</a>  
+        🔮<a href="https://divinescripture.art/">scripture entropy</a>  */} 
       </p>
      );
 }
