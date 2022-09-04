85c6939045fb6610332ea6906328abba5625dbc3
0xdivi <91709492+dev-divi@users.noreply.github.com>
2022-09-04 14:53:42 -0700

    fire

--- pages/MainSite/SiteArt.js
+++ pages/MainSite/SiteArt.js
@@ -1,23 +1,33 @@
 import Image from 'next/image'
 
-import one from '/styles/art_gallery/sfn.png'
 import utilStyles from '/styles/utils.module.css';
 import artStyles from '/styles/art.module.css';
-const One = (props) => {
-  return (
-    <Image
-      src={one}
-      alt=""
-      width=""
-      height=""
-    />
-  )
-  }
+{/* 
+import one from '/styles/art_gallery/sfn.png'
+const One = (props) => { return ( <Image src={one} alt="" width="" height="" /> )}
+*/}
 const SiteArt = () => {
+     {/* <One /> */}
     return (
-        <div className={artStyles.art_one}>
-            {/* <One /> */}
+        <div> 
+          <div className={artStyles.art_one}></div> 
+          <div className={artStyles.art_2}></div>
+          <div className={artStyles.art_3}></div>
+          <div className={artStyles.art_4}></div>
+          <div className={artStyles.art_5}></div>
+          <div className={artStyles.art_6}></div>
+          - Tao Te Ching Wallpapers - 
+          <p> - 01 - </p>
+          <div className={artStyles.art_7}></div>
+          <p> - 02 - </p>
+          <div className={artStyles.art_8}></div>
+          {/* 
+          <div className={artStyles.art_9}></div>
+          <div className={artStyles.art_10}></div>*/}
+
+
         </div> 
+        
      );
 }
  
--- pages/MainSite/SiteFlow.js
+++ pages/MainSite/SiteFlow.js
@@ -4,7 +4,7 @@ const SiteFlow = () => {
     return ( 
         <p>
             <h3> - Flow - </h3>
-            <iframe width="500" height="315" src="https://www.youtube.com/embed/videoseries?list=PLwgC-cD-X2_UIk_Ef7VqSzXQUkDzobqoQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="500" height="315" src="https://www.youtube.com/embed/videoseries?list=PLwgC-cD-X2_UIk_Ef7VqSzXQUkDzobqoQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </p> 
         
     );
