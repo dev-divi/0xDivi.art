3f95f7fab853e00f9a635b6d29f88eb90320edcd
0xdivi <91709492+dev-divi@users.noreply.github.com>
2022-09-06 02:32:56 -0700

    go

--- pages/MainSite/SiteCreativeResources.js
+++ pages/MainSite/SiteCreativeResources.js
@@ -3,23 +3,37 @@ import utilStyles from '/styles/utils.module.css';
 
 const SiteCreativeResources = () => {
     return (
-        <div className={utilStyles}>
+        <div >
             <h3> - Know - </h3>
 
-            <p>  [To be updated with links] <br /> 
+            <p className={utilStyles.resources}>  <br /> 
                 Learn Life Skills <br /> 
-                <ul> 
-                    <li>     - How to Draw </li>
-                    <li>     - How to Write </li>
-                    <li>     - How to Code </li>
-                    <li>     - How to Animate </li> {/* Scratch */}
-                    <li>     - How to Design Games </li>
-                    <li>     - How to Graphic Design </li> {/* GIMP */}
-                    <li>     - How to Nerd </li> {/* Linux */}
-                    <li>     - How to Win - Pareto Principle </li>
-                 {/*}   <li>     - Creative Site List </li>  */}
+                <ul className={utilStyles.black}> 
+                <li><a href="https://youtu.be/_X0mgOOSpLU"> - How to Anything</a> - YouTube</li>
+                <li><a href="https://www.youtube.com/watch?v=ewMksAbgdBI"> - How to Draw</a> - YouTube</li>
+                <li> - How to Code</li>
+                <ul>
+                    <li><a href="https://www.wikihow.com/Code"> - #1 WikiHow</a></li>
+                    <li><a href="https://www.freecodecamp.org/news/how-to-code-coding-for-beginners-and-how-to-learn-programming-for-free/"> - #2 FreeCodeCamp</a></li>
                 </ul>
-                
+
+                <li><a href="https://scratch.mit.edu/">- How to Animate</a> - Scratch</li> 
+                <li><a href="https://learn.unity.com/">- How to Design Games</a> - Unity</li>
+                <li><a href="https://www.youtube.com/watch?v=1MLnP0OiEkw">- How to Graphic Design</a> - YouTube</li>
+                <li><a href="https://youtu.be/NyJWoyVx_XI">- How to 3D Modeling</a> - YouTube</li>
+                <li><a href="https://www.youtube.com/watch?v=d0yGdNEWdn0">- How to Learn Any Language</a> - YouTube</li>
+                <li><a href="https://youtu.be/NyJWoyVx_XI">- How to Nerd</a> - YouTube</li>
+                <li><a href="https://www.amazon.co.uk/Artists-Way-Spiritual-Higher-Creativity">- How to Unlock Creativity</a> - Amazon</li>
+                <li>- How to Win Life</li>
+                <ul> 
+                    <li><a href="https://www.investopedia.com/terms/p/paretoprinciple.asp">Pareto Principle</a> - Investopedia</li> 
+                    <li><a href="https://www.forbes.com/sites/kevinkruse/2016/03/07/80-20-rule/?sh=4b24323a3814">Pareto Principle</a> - Forbes </li>
+                </ul>
+                <li><a href="https://www.youtube.com/watch?v=zmUMhMs5vFE">- Artist without art school   </a>- YouTube</li>
+
+                 {/*}                 - How to Write..   <li>     - Creative Site List </li>  */}
+                </ul>
+
             </p>
         </div> 
      );
--- pages/MainSite/SiteFlow.js
+++ pages/MainSite/SiteFlow.js
@@ -5,6 +5,8 @@ const SiteFlow = () => {
         <p>
             <h3> - Flow - </h3>
             <iframe width="500" height="315" src="https://www.youtube.com/embed/videoseries?list=PLwgC-cD-X2_UIk_Ef7VqSzXQUkDzobqoQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <h3> - Addiction Recovery - </h3> 
+            <iframe width="500" height="315" src="https://www.youtube.com/embed/5szvutMISsw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </p> 
         
     );
